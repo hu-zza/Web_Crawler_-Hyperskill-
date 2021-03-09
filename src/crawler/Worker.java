5b1b1bef3e6deabd3a5c487cb62e9026d33b94e7
Szabó László András <48407681+hu-zza@users.noreply.github.com>
2021-03-09 23:43:53 +0100

    Format according to Google Java Style Guide.

--- src/crawler/Worker.java
+++ src/crawler/Worker.java
@@ -1,29 +1,25 @@
 package crawler;
 
-import static crawler.WebCrawler.fetching;
-import static crawler.WebCrawler.maxDepth;
 import static crawler.WebCrawler.TASKS;
 import static crawler.WebCrawler.executeTask;
+import static crawler.WebCrawler.fetching;
+import static crawler.WebCrawler.maxDepth;
 
-public class Worker implements Runnable
-{
-    @Override
-    public void run()
-    {
-        while (!TASKS.isEmpty() && fetching)
-        {
-            String task = TASKS.poll();
-            
-            if (task != null)
-            {
-                String[] taskParts = task.split("\t", 2);
-                int depth = Integer.parseInt(taskParts[0]);
-                
-                if (maxDepth == 0 || depth <= maxDepth)
-                {
-                    executeTask(taskParts[1], depth);
-                }
-            }
+public class Worker implements Runnable {
+
+  @Override
+  public void run() {
+    while (!TASKS.isEmpty() && fetching) {
+      String task = TASKS.poll();
+
+      if (task != null) {
+        String[] taskParts = task.split("\t", 2);
+        int depth = Integer.parseInt(taskParts[0]);
+
+        if (maxDepth == 0 || depth <= maxDepth) {
+          executeTask(taskParts[1], depth);
         }
+      }
     }
+  }
 }
